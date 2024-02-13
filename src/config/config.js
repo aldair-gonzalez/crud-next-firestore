92bf7bb2bfcf16a2be40500c8aa67fcfe99056b1
Aldair Gonzalez <aldairgome97@gmail.com>
2024-02-13 16:44:21 -0600

    Añadiendo un archivo de configuración

---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+import firebaseConfig from "./firebase.config"
+
+export { firebaseConfig }
--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -1,17 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID
+import { firebaseConfig } from "@/config/config"
+
+const config = {
+  apiKey: firebaseConfig.NEXT_PUBLIC_API_KEY,
+  authDomain: firebaseConfig.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: firebaseConfig.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: firebaseConfig.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: firebaseConfig.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: firebaseConfig.NEXT_PUBLIC_APP_ID
 };
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(config);
 const db = getFirestore(app);
 
 export { app, db }
