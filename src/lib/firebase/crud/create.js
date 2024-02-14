474b8b0add564287f9496230166c3a93971428ac
Aldair Gonzalez <aldairgome97@gmail.com>
2024-02-14 17:55:24 -0600

    Actualizando datos

--- src/lib/firebase/crud/create.js
+++ src/lib/firebase/crud/create.js
@@ -1,23 +1,27 @@
 import { addDoc, collection, getDocs, query } from "firebase/firestore";
 import { db } from "../firebase";
-import { where } from 'firebase/firestore';
-import { Instrumento } from "../models/instrumento.model";
+import { where } from "firebase/firestore";
 
 export const createInstrumento = async (instrumento) => {
   try {
-    const parseInstrumento = new Instrumento(instrumento)
-    if (!parseInstrumento.nombre) throw new Error("El nombre del instrumento es obligatorio");
-    const docRef = collection(db, "pruebas");
-    const q = query(docRef, where("nombre", "==", parseInstrumento.nombre));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach(doc => {
-      if (doc.data().nombre == parseInstrumento.nombre) throw new Error("El instrumento ya existe")
-    })
+    instrumento.nombre = instrumento.nombre.trim();
+    instrumento.nombre = instrumento.nombre.toLowerCase();
 
-    const docSnap = await addDoc(docRef, parseInstrumento);
-    if (docSnap) return {...parseInstrumento, id: docSnap.id}
+    if (!instrumento.nombre)
+      throw new Error("El nombre del instrumento es obligatorio");
+    const docRef = collection(db, "pruebas");
+    const q = query(docRef, where("nombre", "==", instrumento.nombre));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      if (doc.data().nombre == instrumento.nombre)
+        throw new Error("El instrumento ya existe");
+    });
+
+    const docSnap = await addDoc(docRef, instrumento);
+    if (docSnap) return { ...instrumento, id: docSnap.id };
     else throw new Error("Error al crear instrumento");
   } catch (error) {
-    return { error: error.message }
+    console.log(error);
+    return { error: error.message };
   }
-}
+};
