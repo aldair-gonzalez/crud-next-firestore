c7f8b0b2c475ee7ced233da588bd1fbc13ef889c
Aldair Gonzalez <aldairgome97@gmail.com>
2024-02-14 18:02:07 -0600

    Navegación de la página roles

--- src/app/roles/page.jsx
+++ src/app/roles/page.jsx
@@ -1,8 +1,23 @@
-const page = () => {
+"use client"
+
+import { useRouter } from "next/navigation"
+import Link from "next/link"
+
+const Page = () => {
+  const router = useRouter()
+
   return (
-    <section className="flex justify-center items-center">
+    <section className="Section">
       <h1>Roles</h1>
+
+      <ul className="flex items-center justify-center gap-5">
+        <li>
+          <Link href="/roles/all">Ver todos los roles</Link>
+        </li>
+      </ul>
+
+      <button onClick={() => router.back()}>Regresar</button>
     </section>
   )
 }
-export default page
+export default Page
