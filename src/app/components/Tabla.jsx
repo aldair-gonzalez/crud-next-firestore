f9ec42587ac8728d0ae59cd3fd6e22e1ebab91b2
Aldair Gonzalez <aldairgome97@gmail.com>
2024-02-16 13:07:14 -0600

    Añadiendo la propiedad title para el título de la tabla

--- src/app/components/Tabla.jsx
+++ src/app/components/Tabla.jsx
@@ -1,7 +1,7 @@
-const Tabla = ({ children, columns }) => {
+const Tabla = ({ title, children, columns }) => {
   return (
     <>
-      <h1>Instrumentos Registrados</h1>
+      <h1>{title}</h1>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
