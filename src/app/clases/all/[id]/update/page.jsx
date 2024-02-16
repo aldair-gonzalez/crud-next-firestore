97458d345bce0da07167a7193954523397645c1c
Aldair Gonzalez <aldairgome97@gmail.com>
2024-02-15 20:24:21 -0600

    Llamando los datos a la sección actualizar clase

--- src/app/clases/all/[id]/update/page.jsx
+++ src/app/clases/all/[id]/update/page.jsx
@@ -1,16 +1,66 @@
 "use client";
 
+import Loader from "@/app/components/Loader";
+import {
+  getClaseById,
+  getInstrumentoById,
+  getProfesorById,
+  getUsuarioById,
+} from "@/lib/firebase/crud/read";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Page = ({ params }) => {
   const { id } = params;
   const router = useRouter();
 
+  const [loading, setLoading] = useState(true);
+  const [clase, setClase] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const fetch = await getClaseById(id);
+      if (!fetch.error) {
+        fetch.profesor = await getProfesorById(fetch.profesor.id);
+        fetch.profesor.usuario = await getUsuarioById(
+          fetch.profesor.usuario.id
+        );
+        fetch.profesor.instrumento = await getInstrumentoById(
+          fetch.profesor.instrumento.id
+        );
+        setClase(fetch);
+      } else setClase(null);
+      setLoading(false);
+    })();
+  }, [id]);
+
   return (
     <section className="Section">
-      <h1>Actualizar clase {id}</h1>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <h1>
+            Actualizar clase del profesor {clase.profesor.usuario.nombre}{" "}
+            {clase.profesor.usuario.apellido} (
+            {clase.profesor.instrumento.nombre})
+          </h1>
 
-      <button onClick={() => router.back()}>Regresar</button>
+          <ul>
+            <li>
+              fecha: {new Date(clase.fecha.seconds * 1000).getDate()}/
+              {new Date(clase.fecha.seconds * 1000).getMonth()}/
+              {new Date(clase.fecha.seconds * 1000).getFullYear()}
+            </li>
+            <li>hora de inicio: {clase.hora_inicio}</li>
+            <li>hora de fin: {clase.hora_fin}</li>
+            <li>estado: {clase.status}</li>
+          </ul>
+
+          <button onClick={() => router.back()}>Regresar</button>
+        </>
+      )}
     </section>
   );
 };
