ddf76867c9059dac853f6d587d2a44efbee96a68
Aldair Gonzalez <aldairgome97@gmail.com>
2024-02-19 17:49:51 -0600

    Creando el método signUp

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,18 +6,65 @@ import {
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-import { auth } from "./firebase/firebase";
+import { doc, setDoc } from "firebase/firestore";
 import { cookies } from "next/headers";
 
-export const signUp = async ({ email, password }) => {
+import { auth, db } from "./firebase/firebase";
+
+export const signUp = async (user) => {
   try {
+    const { email, password, rolAsigned, ...rest } = user;
     const createdUser = await createUserWithEmailAndPassword(
       auth,
       email,
       password
     );
-    const user = createdUser.user;
-    await sendEmailVerification(user);
+
+    const firebaseUser = createdUser.user;
+    auth.currentUser = firebaseUser;
+
+    // Almacena los datos del usuario en la colección 'usuarios'
+    await setDoc(doc(db, "usuarios", firebaseUser.uid), {
+      email,
+      ...rest,
+      rol: doc(db, "roles", rolAsigned.id),
+    });
+
+    // Decide en qué colección almacenar la referencia del usuario
+    let collection;
+    let additionalData = {};
+    switch (rolAsigned.nombre) {
+      case "profesor":
+        collection = "profesores";
+        // Aquí puedes agregar los campos adicionales para el profesor
+        additionalData = {
+          instrumento: doc(db, "instrumentos", user?.instrumento),
+        };
+        break;
+      case "alumno":
+        collection = "alumnos";
+        // Aquí puedes agregar los campos adicionales para el alumno
+        additionalData = { profesor: doc(db, "profesores", user?.profesor) };
+        break;
+      default:
+        // No necesitamos almacenar una referencia para los administradores
+        return;
+    }
+
+    // Almacena la referencia del usuario y los datos adicionales en la colección correspondiente
+    await setDoc(doc(db, collection, firebaseUser.uid), {
+      usuario: doc(db, "usuarios", firebaseUser.uid),
+      ...additionalData,
+    });
+    await sendEmailVerification(firebaseUser);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const verifyEmail = async () => {
+  try {
+    await sendEmailVerification(auth.currentUser);
   } catch (error) {
     return { error: error.message, code: error.code };
   }
@@ -26,6 +73,7 @@ export const signUp = async ({ email, password }) => {
 export const signIn = async ({ email, password }) => {
   try {
     const userSigned = await signInWithEmailAndPassword(auth, email, password);
+    auth.currentUser = userSigned.user;
     const user = {
       token: userSigned.user.accessToken,
       email: userSigned.user.email,
