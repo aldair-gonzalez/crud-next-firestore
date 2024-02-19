e94ab94eaf6d20bd050d86062d726769f5879d52
Aldair Gonzalez <aldairgome97@gmail.com>
2024-02-19 17:50:26 -0600

    Se pueden enviar un email de verificación de cuenta

--- src/app/email-verified/page.jsx
+++ src/app/email-verified/page.jsx
@@ -1,11 +1,25 @@
+"use client";
+
+import { verifyEmail } from "@/lib/auth";
 import Link from "next/link";
 
 const page = () => {
+  const handleVerifyEmail = async () => {
+    await verifyEmail();
+  };
+
   return (
-    <div>
+    <section className="Section">
       No has verificado tu cuenta
-      <Link href="/sign-in">Iniciar Sesion</Link>
-    </div>
+      <div className="text-xs grid grid-cols-1 gap-2">
+        <button onClick={() => handleVerifyEmail()}>
+          Enviar correo de verificación
+        </button>
+        <Link href="/sign-in">
+          Inicia sesión nuevamente con tu cuenta verificada
+        </Link>
+      </div>
+    </section>
   );
 };
 export default page;
