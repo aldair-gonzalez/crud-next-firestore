3dd00c74b96df022e880682ac051165554f5a80a
Aldair Gonzalez <aldairgome97@gmail.com>
2024-03-03 18:07:52 -0600

    Parseando los schemas

--- src/lib/firebase/schemas.js
+++ src/lib/firebase/schemas.js
@@ -1,3 +1,18 @@
+import { doc } from "firebase/firestore";
+import { db } from "./firebase";
+
+const Utils = {
+  toTrimLowercase(string) {
+    return string.trim().toLowerCase();
+  },
+  toTrimUppercase(string) {
+    return string.trim().toUpperCase();
+  },
+  toTrim(string) {
+    return string.trim();
+  },
+};
+
 export class Rol {
   constructor({ nombre }) {
     this.nombre = nombre;
@@ -6,24 +21,43 @@ export class Rol {
 
 export class Instrumento {
   constructor({ nombre }) {
-    this.nombre = nombre;
+    this.nombre = Utils.toTrimLowercase(nombre);
   }
 }
 
 export class Usuario {
   constructor({ nombre, apellido, email, telefono, rol }) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.email = email;
-    this.telefono = telefono;
-    this.rol = rol;
+    this.full_name = this.parseFullName({ nombre, apellido });
+    this.email = Utils.toTrim(email);
+    this.telefono = Utils.toTrim(telefono);
+    this.rol = this.parseRolRef({ rol });
+  }
+
+  parseFullName({ nombre, apellido }) {
+    nombre = Utils.toTrim(nombre);
+    apellido = Utils.toTrim(apellido);
+    return { nombre, apellido };
+  }
+
+  parseRolRef({ rol }) {
+    return doc(db, "roles", rol.id);
   }
 }
 
 export class Profesor {
   constructor({ usuario, instrumentos }) {
-    this.usuario = usuario;
-    this.instrumentos = instrumentos;
+    this.usuario = this.parseUsuarioRef(usuario);
+    this.instrumentos = this.parseInstrumentosRef({ instrumentos });
+  }
+
+  parseUsuarioRef({ usuario }) {
+    return doc(db, "usuarios", usuario.id);
+  }
+
+  parseInstrumentosRef({ instrumentos }) {
+    return instrumentos.map((instrumento) => {
+      return doc(db, "instrumentos", instrumento.id);
+    });
   }
 }
 
@@ -32,24 +66,50 @@ export class Clase {
     this.fecha = fecha;
     this.hora_inicio = hora_inicio;
     this.hora_fin = hora_fin;
-    this.profesor = profesor;
-    this.instrumento = instrumento;
+    this.profesor = this.parseProfesorRef({ profesor });
+    this.instrumento = this.parseInstrumentoRef({ instrumento });
     this.status = status;
+  }
+
+  parseProfesorRef({ profesor }) {
+    return doc(db, "profesores", profesor.id);
+  }
+
+  parseInstrumentoRef({ instrumento }) {
+    return doc(db, "instrumentos", instrumento.id);
   }
 }
 
 export class Alumno {
   constructor({ usuario, profesor, instrumentos }) {
-    this.usuario = usuario;
-    this.profesor = profesor;
-    this.instrumentos = instrumentos;
+    this.usuario = this.parseUsuarioRef({ usuario });
+    this.profesor = this.parseProfesorRef({ profesor });
+    this.instrumentos = this.parseInstrumentosRef({ instrumentos });
+  }
+
+  parseUsuarioRef({ usuario }) {
+    return doc(db, "usuarios", usuario.id);
+  }
+
+  parseProfesorRef({ profesor }) {
+    return doc(db, "profesores", profesor.id);
+  }
+
+  parseInstrumentosRef({ instrumentos }) {
+    return instrumentos.map((instrumento) => {
+      return doc(db, "instrumentos", instrumento.id);
+    });
   }
 }
 
 export class AlumnoAsistencia {
   constructor({ clase, status }) {
-    this.clase = clase;
+    this.clase = this.parseClaseRef({ clase });
     this.status = status;
+  }
+
+  parseClaseRef({ clase }) {
+    return doc(db, "clases", clase.id);
   }
 }
 
